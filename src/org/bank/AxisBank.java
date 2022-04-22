411f19d2dba5a480ecd6a9dba61308755519fbc7
Mohamedshafi11 <mohamedshafibca@gmail.com>
2022-04-22 16:32:16 +0530

    new code change

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -4,7 +4,7 @@ public class AxisBank extends BankInfo {
 	@Override
 	public void deposit() {
 		System.out.println("Deposit amount is 15000.00 ");
-
+		System.out.println("Deposit amount is 188000.00 ");
 	}
 
 	public static void main(String[] args) {
