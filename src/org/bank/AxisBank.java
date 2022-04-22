a04c793e91f621ea830d324938906c743fab30de
Mohamedshafi11 <mohamedshafibca@gmail.com>
2022-04-22 16:26:01 +0530

    new bank

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class AxisBank extends BankInfo { 
+	@Override
+	public void deposit() {
+		System.out.println("Deposit amount is 15000.00 ");
+
+	}
+
+	public static void main(String[] args) {
+		AxisBank l = new AxisBank();
+		l.deposit();
+		l.saving();
+		l.fixed();
+		
+
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+System.out.println("Savings Account is 5000.00");
+	}
+	public void fixed() {
+System.out.println("Fixed Deposit is 8000.00");
+
+	}
+	public void deposit() {
+System.out.println(" Deposit is 9000.00");
+
+	}
+}
